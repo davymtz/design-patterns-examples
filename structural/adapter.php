f0df1deaf8d78327797834fe548441a1552dc909
juanpago <juan.carlos@pagofacil.net>
2017-10-26 02:15:39 -0500

    Adapter pattern

--- structural/adapter.php
+++ structural/adapter.php
@@ -1,9 +1,45 @@
 <?php
-
+interface IPaymentAdapter {
+    public function pay($amount);
+}
 class FooPay {
     public function makePayment($amount){
-        echo "FooPay: $amount";
+        echo "FooPay: $amount \n";
     }
 }
 
-$gateway = new FooPay();
+class BarPayment {
+    public function payment($amount){
+        echo "FooPay: $amount \n";
+    }
+}
+
+class FooPaymentAdapter implements IPaymentAdapter {
+    public $fooPay;
+    public function __construct(FooPay $fooPay)
+    {
+        $this->fooPay = $fooPay;
+    }
+
+    public function pay($amount)
+    {
+        $this->fooPay->makePayment($amount);
+    }
+}
+class BarPaymentAdapter implements IPaymentAdapter {
+    public $barPay;
+    public function __construct(BarPayment $barPay)
+    {
+        $this->barPay = $barPay;
+    }
+
+    public function pay($amount)
+    {
+        $this->barPay->payment($amount);
+    }
+}
+
+$gateway = new FooPaymentAdapter(new FooPay());
+$gateway->pay(100);
+$gateway = new BarPaymentAdapter(new BarPayment());
+$gateway->pay(200);
